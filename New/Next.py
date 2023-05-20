9125c5194890999e45894b29c7cdea10c7883db3
Spooky-smile <z20211030@outlook.com>
2023-05-20 17:26:05 +0800

    message

---
+++ New/Next.py
@@ -0,0 +1,10 @@
+import random
+print('感谢使用本程序，期望他能解决您的困扰。')
+mun1 = input('请键入计划一：')
+mun2 = input('请键入计划二：')
+end = random.randint(1, 2)
+if end == 1:
+    print('你的计划是', mun1)
+else:
+    print('你的计划是', mun2)
+what = input("Let's to do, just now." )
